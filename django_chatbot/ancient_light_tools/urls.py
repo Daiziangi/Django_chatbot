16a5352adf023173f0033d08b1c95d4ec8068988
dza <dza@qq.com>
2024-03-27 20:03:07 +0800

    在古文轻应用中添加了一个用于将现代文转换成古文的页面功能.   尝试了新的大语言模型,但是没有预设的功能,目前还没有用上.

--- django_chatbot/ancient_light_tools/urls.py
+++ django_chatbot/ancient_light_tools/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('sentence_split', views.sentence_split, name='sentence_split'),
+    path('modern_to_ancient',views.modern_to_ancient,name="modern_to_ancient"),
 
 ]
--- django_chatbot/ancient_light_tools/views.py
+++ django_chatbot/ancient_light_tools/views.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, BertForTokenClassification
 from transformers import pipeline
 
 
-
+#古长文分句
 TAG = "raynardj/classical-chinese-punctuation-guwen-biaodian"
 ner = pipeline("ner", model=TAG, tokenizer=TAG)
 
@@ -25,3 +25,45 @@ def sentence_split(request):
 
     else:
         return render(request,'split.html')
+    
+
+
+#现代文转古文
+from transformers import (
+  EncoderDecoderModel,
+  AutoTokenizer
+)
+PRETRAINED = "raynardj/wenyanwen-chinese-translate-to-ancient"
+tokenizer = AutoTokenizer.from_pretrained(PRETRAINED)
+model = EncoderDecoderModel.from_pretrained(PRETRAINED)
+
+import torch
+
+def inference(text):
+    tk_kwargs = dict(
+      truncation=True,
+      max_length=128,
+      padding="max_length",
+      return_tensors='pt')
+   
+    inputs = tokenizer([text,],**tk_kwargs)
+    with torch.no_grad():
+        return tokenizer.batch_decode(
+            model.generate(
+            inputs.input_ids,
+            attention_mask=inputs.attention_mask,
+            num_beams=3,
+            max_length=128,
+            bos_token_id=101,
+            eos_token_id=tokenizer.sep_token_id,
+            pad_token_id=tokenizer.pad_token_id,
+        ), skip_special_tokens=True)
+
+def modern_to_ancient(request):
+    if request.method == 'POST':
+        modern_sentence = request.POST.get('modern_sentence')
+        ancient_sentence = inference(modern_sentence)
+        return render(request, 'modern2ancient.html', {'ancient_sentence': ancient_sentence})
+
+    else:
+        return render(request,'modern2ancient.html')
